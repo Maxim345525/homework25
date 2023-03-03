822724cde39a5ee82a6803a8b28d25cfc2c42251
Maxim345525 <maximmaksimchyk@gmail.com>
2023-03-03 17:04:52 +0200

    add code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
-d
+#include<iostream>
+using namespace std;
+void addToEnd(int** array, int numRows, int numCols, int* rowToAdd) {
+    int** newArray = new int* [numRows + 1];  
+    for (int i = 0; i < numRows; i++) {
+        newArray[i] = array[i];  
+    }
+    newArray[numRows] = rowToAdd; 
+    delete[] array;
+    array = newArray;
+    cout << newArray << endl;
+}
